ce38276a56905a280b3167f2c9d01755db6a021b
sttadic <stjepan.tadich@gmail.com>
2025-03-14 13:30:32 +0000

    Implement functionalty to get list of books by author name

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,8 +26,17 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { author } = req.params;
+  let bookByAuthor = [];
+  for (let i in books) {
+    if (books[i].author === author) {
+      bookByAuthor.push(books[i]);
+    }
+  }
+  if (bookByAuthor.length > 0) {
+    return res.status(200).json(bookByAuthor);
+  }
+  return res.status(404).json({ message: "No books found!" });
 });
 
 // Get all books based on title
