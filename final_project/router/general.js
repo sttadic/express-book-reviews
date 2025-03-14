911ba84602066aa302ac405b0a35c3690031ed38
sttadic <stjepan.tadich@gmail.com>
2025-03-14 13:44:59 +0000

    Add logic to get the books by title and reviews by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,14 +41,27 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { title } = req.params;
+  let bookByTitle = [];
+  for (let i in books) {
+    if (books[i].title === title) {
+      bookByTitle.push(books[i]);
+    }
+  }
+  if (bookByTitle.length > 0) {
+    return res.status(200).json(bookByTitle);
+  }
+  return res.status(404).json({ message: "No books found!" });
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
